da87be365bf8ad5b4426622895ad89371f3cbfcc
Kritika Berry <kritika@ycorp.in>
2021-07-12 11:03:20 +0530

    Base template for converting angular websites to ionic app

---
+++ platforms/android/app/src/main/assets/www/9605.aefa9233bcfc4d2c1ca5.js
@@ -0,0 +1,1 @@
+(self.webpackChunkproj=self.webpackChunkproj||[]).push([[9605],{9605:(o,s,r)=>{"use strict";r.r(s),r.d(s,{ion_text:()=>e});var t=r(2322),c=r(7505),l=r(4843);const e=class{constructor(o){(0,t.r)(this,o)}render(){const o=(0,c.b)(this);return(0,t.h)(t.H,{class:(0,l.c)(this.color,{[o]:!0})},(0,t.h)("slot",null))}};e.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
